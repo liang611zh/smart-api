c0d5119b5094d69889925dc1b57d590b205e5154
victor zhao <zhaoliangbz@gmail.com>
2018-02-23 12:10:00 -0800

    add heroku database

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const entries = require('./controllers/entries');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'liangzhao',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
